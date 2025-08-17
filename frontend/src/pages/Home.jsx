8bf5e1e6516d5e58c29913e6cfed82d39fa9120c
Rodela Ghosh <ghoshrodela111@gmail.com>
2025-08-16 21:46:51 -0400

    Added link to deployed backend

--- frontend/src/pages/Home.jsx
+++ frontend/src/pages/Home.jsx
@@ -35,7 +35,7 @@ const Home = () => {
     };
 
     try {
-      const response = await fetch("http://127.0.0.1:5000/predict", {
+      const response = await fetch("https://efficiency-predictor-backend.onrender.com/predict", {
         method: "POST",
         headers: { "Content-Type": "application/json" },
         body: JSON.stringify(boundedData),
